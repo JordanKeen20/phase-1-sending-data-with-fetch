6daff1683b17f468c7cfc90fbd44510e1716cd53
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-07-26 09:45:52 -0700

    commit tree e021445e87011b911795a5f6a8ba502e942a49aa

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name, 
+            email })
+    })
+    .then(response => response.json())
+    .then(object => {
+        console.log(object)
+        console.log(object.id)
+        const body = document.querySelector('body');
+        const p = document.createElement ('p');
+        p.textContent = object.id;
+        body.append(p);
+    })
+
+    .catch(error => {
+        console.log(error.message)
+        const body = document.querySelector('body');
+        const p = document.createElement('p');
+        p.textContent = error.message;
+        body.append(p)
+    });
+};
+
+    submitData('Steve', 'steven@steven.com');
